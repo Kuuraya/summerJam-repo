dca13bda5f2248e2fc8fbf1a7e53909ffb4e308a
Josh <joshdgreaves@gmail.com>
2019-05-26 14:07:51 -0400

    First Real Push With Files
    This contains multiple different prototypes and a lot of probably not going to get used sample assets/scenes. This will get cleaned out in the near future.

---
+++ SummerJam/Assets/Scripts/climbWallScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class climbWallScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log("YouShouldBeClimbing");
+        if (other.attachedRigidbody)
+            //other.attachedRigidbody.AddForce(Vector3.up * 0);
+            other.attachedRigidbody.velocity = new Vector3(0, -1.0F, 0);
+            Physics.gravity = new Vector3(0, -1.0F, 0);
+    }
+}
